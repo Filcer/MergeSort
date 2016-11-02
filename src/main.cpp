f171765c77bc70ae9d85b3568d762fca43b1e13e
Filcer <gabfiltzer@gmail.com>
2016-11-01 23:10:36 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,6 @@
-##include "Lista.h"
+//#include 
 
 int int main()
 {
-	Lista l = Lista(3);
-	l.AddInicio(4);
-	l.AddInicio(5);
-	l.AddInicio(6);
-	l.Show();
-	std::cout<<"hello World<<"<<std::endl;
-	return 0;
+	
 }
