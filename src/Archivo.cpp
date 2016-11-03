fcc0a6ecd8a822b624a0401f25ccea87e6a64f7b
Filcer <gabfiltzer@gmail.com>
2016-11-03 01:16:57 -0600

    Update Archivo.cpp

--- src/Archivo.cpp
+++ src/Archivo.cpp
@@ -1,13 +1,38 @@
 
 #include "Archivo.h"
+#include <vector>
 
-void Archivo::Leer(){
-
+ 
+   array Archivo::Leer(){
+   int arr[29];  
+   archivo.open("test_merge.txt", ios::out | ios::in);
+            if (archivo.is_open()) {
+                for(int i = 0; i < 29; i++)
+                    cout << arr[i] << " ";
+                archivo.close();
+                return arr;
+            } else
+                cout << "No se pudo abrir el archivo";
 }
 
 
-void Archivo::Escribir(){
+array Archivo::Escribir(){ //escribir el arreglo nuevo hecho en mergesort al archivo 
+ 
+  archivo.open("test_merge.txt", ios::out | ios::in); 
+            if (archivo.is_open()) {
+               // while (getline(archivo, linea)) {
+                 //   cout << linea << endl;
+                }
+                do {
+                    cin >> entrada;
+                    archivo << entrada << endl;
+                } while (entrada != '\n');
 
+  
+  for(int i = 0; i < 29; i++)
+                    cout << arr[i] << " ";
+                archivo.close();
+                return arr;
 
 
 }
