49a7c72fca943e151410e2d1795dd899f927c28c
Filcer <gabfiltzer@gmail.com>
2016-11-03 23:22:50 -0600

    Revisar

--- src/Archivo.cpp
+++ src/Archivo.cpp
@@ -20,19 +20,12 @@ array Archivo::Escribir(){ //escribir el arreglo nuevo hecho en mergesort al arc
  
   archivo.open("test_merge.txt", ios::out | ios::in); 
             if (archivo.is_open()) {
-               // while (getline(archivo, linea)) {
-                 //   cout << linea << endl;
+                while (!archivo.eof()) {
+                 getline(archivo, linea)   
+                 arr[i++] << linea;
                 }
-                do {
-                    cin >> entrada;
-                    archivo << entrada << endl;
-                } while (entrada != '\n');
-
-  
-  for(int i = 0; i < 29; i++)
-                    cout << arr[i] << " ";
                 archivo.close();
-                return arr;
-
-
+            } else
+                cout << "No se pudo abrir el archivo" << endl;
+    return arr; //¿?
 }
