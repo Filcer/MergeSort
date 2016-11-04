37d88f64262b6778176aed32f33de3b53ab2fcdd
Filcer <gabfiltzer@gmail.com>
2016-11-03 22:53:46 -0600

    Falta leer archivo y guardarlo en arreglo

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,21 @@
 int main()
 {
 	//hay que leer el archivo 
-	//meter el archivo en un arreglo
-	//pasarle el arreglo a mergesort
-	//sucede mergesort (regresa un arreglo)
-	//imprimir el arreglo resultante
+	//meter el archivo en un arreglo (a)
 	
-	
+	/* int a[20], i;
+    cout<<"Enter 5 elements to sort : \n";
+    for (i = 0; i < 5; i++)
+    {
+        cin>>a[i];
+    }*/ 
+    mergesort(a, 0, 160); //'a' es el arreglo en el que va lo que se lee del archivo
+
+    for (i = 0; i < 5; i++)
+    {
+        cout<<a[i]<<"\t"; //aquí en vez de imprimir hay que escribirlo en el archivo
+    }
+
+    return 0;	
 	
 }
